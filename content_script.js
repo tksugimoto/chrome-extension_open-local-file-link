e79ba2356ca6753a0bc8ce758c5994bcd0e8feec
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-01-12 13:47:07 +0900

    <a>の中に<img>などが含まれている場合に<a>クリックとして検知されないバグ修正

--- content_script.js
+++ content_script.js
@@ -1,6 +1,9 @@
 
 document.body.addEventListener("click", function (evt) {
     var target = evt.target;
+    while (target.parentNode && target.tagName !== "A") {
+        target = target.parentNode;
+    }
     if (target.tagName === "A") {
         var path = target.href;
         if (path.lastIndexOf("file://", 0) === 0) {
