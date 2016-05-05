cfcafc54101e278faf117664107bf36af2fc2f9e
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-05-05 12:59:28 +0900

    [Update] Eventページ対応
    メモリ使用量の削減

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,17 +1,19 @@
 "use strict";
 
-// 読み込み/更新時に既存のタブで実行する
-chrome.tabs.query({
-	url: "*://*/*"
-}, function(result){
-	result.forEach(function (tab){
-		chrome.tabs.executeScript(tab.id, {
-			file: "content_script.js",
-			allFrames: true
-		}, function (result) {
-			if (typeof result === "undefined") {
-				console.info("ページが読み込まれていません", tab);
-			}
+chrome.runtime.onInstalled.addListener(function (details) {
+	// 読み込み/更新時に既存のタブで実行する
+	chrome.tabs.query({
+		url: "*://*/*"
+	}, function(result){
+		result.forEach(function (tab){
+			chrome.tabs.executeScript(tab.id, {
+				file: "content_script.js",
+				allFrames: true
+			}, function (result) {
+				if (typeof result === "undefined") {
+					console.info("ページが読み込まれていません", tab);
+				}
+			});
 		});
 	});
 });
