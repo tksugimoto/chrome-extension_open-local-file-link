6cc92bebbf4975c4e1961e52a161d7f25233d1c0
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-02-17 20:27:35 +0900

    [Clean] リファクタリング: インデントをSpace4個からTabに変更

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,24 +1,24 @@
 
 document.body.addEventListener("click", function (evt) {
-    // ユーザーの操作によるイベントならisTrusted == true
-    // Chrome 46.0～
-    // https://developer.mozilla.org/ja/docs/Web/API/Event/isTrusted
-    if (!evt.isTrusted) return;
-    var target = evt.target;
-    while (target.parentNode && target.tagName !== "A") {
-        target = target.parentNode;
-    }
-    if (target.tagName === "A") {
-        var path = target.href;
-        if (path.lastIndexOf("file://", 0) === 0) {
-            evt.preventDefault();
-            // 拡張が再読み込みされた場合エラーになるので捕捉
-            try {
-                chrome.runtime.sendMessage({
-                    method: "openLocalFile",
-                    path: path
-                });
-            } catch (e) {}
-        }
-    }
+	// ユーザーの操作によるイベントならisTrusted == true
+	// Chrome 46.0～
+	// https://developer.mozilla.org/ja/docs/Web/API/Event/isTrusted
+	if (!evt.isTrusted) return;
+	var target = evt.target;
+	while (target.parentNode && target.tagName !== "A") {
+		target = target.parentNode;
+	}
+	if (target.tagName === "A") {
+		var path = target.href;
+		if (path.lastIndexOf("file://", 0) === 0) {
+			evt.preventDefault();
+			// 拡張が再読み込みされた場合エラーになるので捕捉
+			try {
+				chrome.runtime.sendMessage({
+					method: "openLocalFile",
+					path: path
+				});
+			} catch (e) {}
+		}
+	}
 });
