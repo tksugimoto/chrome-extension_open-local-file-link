933702055f1e5682abeadd58b7c8962d975c053f
Dorian <dorianok@users.noreply.github.com>
2020-01-25 18:02:16 +0100

    add svg a tag support (#11)
    * add svg a tag support
    
    * refactor: Remove unnecessary diffs
    
    * refactor: SVG check (SVGAElement)
    
    * refactor: Remove unnecessary url.startsWith check
    
    * fix: Fixed a mistake in the check logic of <a> element.
    
    The #document was judged as target even though it was not <a>.
    
    Co-authored-by: Takashi Sugimoto <tksugimoto@users.noreply.github.com>

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -6,11 +6,12 @@ document.body.addEventListener('click', evt => {
 	// https://developer.mozilla.org/ja/docs/Web/API/Event/isTrusted
 	if (!evt.isTrusted) return;
 	let target = evt.target;
-	while (target && target.tagName !== 'A') {
-		target = target.parentNode;
+	while (target && target.tagName.toLowerCase() !== 'a') {
+		target = target.parentElement;
 	}
 	if (target) {
-		const url = target.href;
+		// check for baseVal of svg a tag's href-SVGAnimatedString
+		const url = target instanceof SVGAElement ? target.href.baseVal : target.href;
 		if (url.startsWith('file://')) {
 			evt.preventDefault();
 			// 拡張が再読み込みされた場合エラーになるので捕捉
